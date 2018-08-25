1a750356d781b89fcccafe49571ce7d0b5d49f84
ydornala <yeshwanth.dornala@objectedge.com>
2018-08-25 07:34:10 +0530

    Initial commit with konnex-app logo animation added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './konnex-logo.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>Loading...</div>
+      </div>
+    );
+  }
+}
+
+export default App;
