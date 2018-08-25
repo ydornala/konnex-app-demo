c57cf27967928ca48c3e136396e50a42196efdc5
ydornala <yeshwanth.dornala@objectedge.com>
2018-08-25 08:46:33 +0530

    Deploy worked with Travis CI and Heroku

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      heroku travis test
         <img src={logo} className="App-logo" alt="logo" />
         <div>Loading...</div>
       </div>
