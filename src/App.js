697e6366e448191dc95a06ae2929df417a906260
ydornala <yeshwanth.dornala@objectedge.com>
2018-08-25 08:25:43 +0530

    travis deploy Added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      heroku travis test
         <img src={logo} className="App-logo" alt="logo" />
         <div>Loading...</div>
       </div>
