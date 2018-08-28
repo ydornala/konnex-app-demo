37d550d86b36ad9786bb1ff79717bee603f774fd
ydornala <yeshwanth.dornala@objectedge.com>
2018-08-28 15:59:36 +0530

    bootstrap added, and one more variation added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import logo from './konnex-logo.jpg';
+import svglogo from './konnex-logo.svg';
+import svglogo_2 from './konnex-logo-2.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>Loading...</div>
+      <div className="App container">
+        <div className="row">
+          <div className="col">
+            <div className="variation-1">
+              <img src={logo} className="App-logo" alt="logo" />
+              <div>Loading...</div>
+            </div>
+          </div>
+      
+          <div className="col d-none">
+            <div className="variation-2">
+              <img src={svglogo} className="App-logo" alt="logo" />
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="variation-3">
+              <img src={svglogo_2} className="App-logo" alt="logo" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
