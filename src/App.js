a5e3a0b172d3820f136fb0dc6176e6447ae463b1
ydornala <yeshwanth.dornala@objectedge.com>
2018-09-14 16:07:30 +0530

    removed unused

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-import logo from './konnex-logo.jpg';
-import svglogo from './konnex-logo.svg';
-import svglogo_2 from './konnex-logo-2.svg';
-import svglogo_3 from './konnex-with-beat.svg';
-import svg_loading from './konnex-with-beat_copy.svg';
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
